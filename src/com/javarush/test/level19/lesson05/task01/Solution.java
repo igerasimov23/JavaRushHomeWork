516e6a6900126b6aab98f66e22ad0c7a97389a5e
Ilya Gerasimov <IGERASIM@cablevision.com>
2016-09-06 17:10:47 -0400

    level9 begin

--- src/com/javarush/test/level19/lesson05/task01/Solution.java
+++ src/com/javarush/test/level19/lesson05/task01/Solution.java
@@ -7,7 +7,35 @@ package com.javarush.test.level19.lesson05.task01;
 Закрыть потоки ввода-вывода.
 */
 
+import java.io.*;
+import java.util.ArrayList;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String file1 = br.readLine();
+        String file2 = br.readLine();
+
+
+        FileReader reader = new FileReader(file1);
+        FileWriter writer = new FileWriter(file2);
+        ArrayList<Integer> list = new ArrayList<>();
+
+        while(reader.ready()){
+            int data = reader.read();
+            list.add(data);
+
+        }
+
+        for(int i = 0; i < list.size(); i ++){
+            if(i % 2 == 1){
+                writer.write(list.get(i));
+            }
+        }
+
+
+        reader.close();
+        writer.close();
+        br.close();
     }
 }
