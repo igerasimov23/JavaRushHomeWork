2f3d7b03270ecd90ffb2fac6a777e719988541e9
Ilya Gerasimov <IGERASIM@cablevision.com>
2017-02-26 18:13:23 -0500

    adding  ignore git

---
+++ src/com/javarush/test/level20/lesson10/home02/Solution.java
@@ -0,0 +1,25 @@
+package com.javarush.test.level20.lesson10.home02;
+
+import java.io.ObjectInputStream;
+import java.io.Serializable;
+
+/* Десериализация
+На вход подается поток, в который записан сериализованный объект класса A либо класса B.
+Десериализуйте объект в методе getOriginalObject предварительно определив, какого именно типа там объект.
+Реализуйте интерфейс Serializable где необходимо.
+*/
+public class Solution {
+    public A getOriginalObject(ObjectInputStream objectStream) {
+        if ()
+        return null;
+    }
+
+    public class A implements Serializable{
+    }
+
+    public class B extends A {
+        public B() {
+            System.out.println("inside B");
+        }
+    }
+}
